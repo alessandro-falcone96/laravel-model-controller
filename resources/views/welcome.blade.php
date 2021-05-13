abf018cb685a8611c733a821d8ca19348c58442b
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-05-13 16:55:54 +0200

    Aggiunto esercizio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,55 +11,8 @@
 
         <!-- Styles -->
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
+            li{
+                list-style-type: none;
             }
         </style>
     </head>
@@ -80,20 +33,16 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <ul>
+                    @foreach ($movies as $movie)
+                        <li>
+                            <h2>{{$movie->title}}</h2>
+                            <h4>{{$movie->author}}</h4>
+                            <h6>{{$movie->genre}}</h6>
+                            <p>{{$movie->plot}}</p>
+                        </li>
+                    @endforeach
+                </ul>
             </div>
         </div>
     </body>
